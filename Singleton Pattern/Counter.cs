de71aa9e7dc64538e1302880c690a486267da2e8
Elamir-Nady <ElamirNady@gmail.com>
2021-12-26 23:54:34 +0200

    Singleton-Pattern---Lazy-Initialization

---
+++ Singleton Pattern/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Pattern
+{
+    public class Counter
+    {
+        public int count = 0;
+        private static Counter instace = null;
+
+        private Counter()
+        {
+        }
+        public static Counter GetInstance()
+        {
+            if(instace == null)
+            {
+                new Counter();
+            }
+            return instace;
+        }
+        public void Addone()
+        {
+            count++;
+        }
+    }
+}
---
+++ Singleton Pattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Singleton_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 =  Counter.GetInstance();
+            Counter counter2 = Counter.GetInstance();
+            counter1.Addone();
+            counter2.Addone();
+            Console.WriteLine("counter1 : " + counter1.count.ToString());
+            Console.WriteLine("counter2 : " + counter1.count.ToString());
+            Console.WriteLine();
+            
+            counter1.Addone();
+            Console.WriteLine("counter1 : " + counter1.count.ToString());
+            Console.WriteLine("counter2 : " + counter1.count.ToString());
+
+        }
+    }
+}
